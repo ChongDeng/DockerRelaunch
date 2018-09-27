b0dd007c0ae0ce57eef54e07ae93099208e262bf
chongdeng <fqyyang@163.com>
2018-09-26 21:44:58 -0700

    docker compose demo with 1 HAproxy and 5 flask server

---
+++ demo5_compose_HAProxy_flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', port = 80)
+
+
